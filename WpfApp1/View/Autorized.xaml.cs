be29401265b9ba9501b81c644fb364c001dff040
pushechkin524 <134803783+pushechkin524@users.noreply.github.com>
2024-06-08 22:38:37 +0300

    Реализовано окно авторизации и начато окно пациента

---
+++ WpfApp1/View/Autorized.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinalTenthPractical.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorized.xaml
+    /// </summary>
+    public partial class Autorized : Window
+    {
+        public Autorized()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            FramePatientAutorize.NavigationService.Navigate(new FrameAutorizedPatient());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ WpfApp1/View/FrameAutorizedPatient.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalTenthPractical.View
+{
+    /// <summary>
+    /// Логика взаимодействия для FrameAutorizedPatient.xaml
+    /// </summary>
+    public partial class FrameAutorizedPatient : Page
+    {
+        public FrameAutorizedPatient()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "Номер полиса")
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Номер полиса";
+                textBox.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void TBXPolis_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            foreach (char c in e.Text)
+            {
+                if (!char.IsDigit(c))
+                {
+                    e.Handled = true;
+                    ERRORtbx.Text = "Можно использовать только цифры";
+                    return;
+                }
+            }
+            ERRORtbx.Text = string.Empty;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var parentWindow = Window.GetWindow(this);
+
+            if (parentWindow != null && parentWindow is Autorized autorized)
+            {
+                autorized.FramePatientAutorize.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
+                autorized.FramePatientAutorize.Content = new FrameAutorizedDoctor();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            Patient patient = new Patient();
+            patient.Show();
+
+        }
+    }
+}
