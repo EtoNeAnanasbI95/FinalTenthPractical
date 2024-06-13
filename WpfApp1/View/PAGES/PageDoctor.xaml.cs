87c6df1fcd913acbb6cc14a1180c925027d16459
pushechkin524 <134803783+pushechkin524@users.noreply.github.com>
2024-06-13 19:09:45 +0300

    sdf

--- WpfApp1/View/PAGES/PageDoctor.xaml.cs
+++ WpfApp1/View/PAGES/PageDoctor.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using FinalTenthPractical.View.USERCONTROLS;
+using Spire.Doc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +26,34 @@ namespace FinalTenthPractical.View.PAGES
         public PageDoctor()
         {
             InitializeComponent();
+
+            Document doc = new Document();
+            doc.LoadFromFile(@"C:\Users\Ilyam\Downloads\РЕЗУЛЬТАТЫ_ЛАБЮОРАТОРНЫХ_ИССЛЕДОВАНИЙ.docx");
+            doc.SaveToFile("конвертировать.rtf", FileFormat.Rtf);
+
+            var range = new TextRange(RTBone.Document.ContentStart, RTBone.Document.ContentEnd);
+            var fs = new FileStream("конвертировать.rtf", FileMode.OpenOrCreate);
+            range.Load(fs, DataFormats.Rtf);
+            fs.Close();
+
             DoctorWay first = new DoctorWay();
             first.TxtBlock.Text = "Ебать в рот";
+            
+            DoctorWay second = new DoctorWay();
+            first.TxtBlock.Text = "Ебать в рот";
 
-            List<DoctorWay> users = new List<DoctorWay>() { first};
-            ListSecond.ItemsSourse = users;
+            DoctorWay th = new DoctorWay();
+            first.TxtBlock.Text = "Ебать в рот";
+
+            List<DoctorWay> users = new List<DoctorWay>() {first, second, th };
+            ListSecond.ItemsSource = users;
         }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+      
     }
 }
