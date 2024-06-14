b674d896694232d953ca0befd33f54bc4b54c765
EtoNeAnanasbI95 <ananasovii.yashik@gmail.com>
2024-06-14 20:34:52 +0300

    добавил окно с выходами во все другие места приложения ради удобства

---
+++ WpfApp1/View/DEVELOPING_WINDOW.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using FinalTenthPractical.View.WINDOWS;
+
+namespace FinalTenthPractical.View;
+
+public partial class DEVELOPING_WINDOW : Window
+{
+    public DEVELOPING_WINDOW()
+    {
+        InitializeComponent();
+    }
+
+    private void GO_ADMIN(object sender, RoutedEventArgs e)
+    {
+        AdministratorWindow admin = new AdministratorWindow();
+        admin.Show();
+        this.Close();
+    }
+
+    private void GO_AUTH(object sender, RoutedEventArgs e)
+    {
+        AuthorizationWindow windows = new AuthorizationWindow();
+        windows.Show();
+        this.Close();
+    }
+
+    private void GO_DOCTOR(object sender, RoutedEventArgs e)
+    {
+        DoctorWindow windows = new DoctorWindow();
+        windows.Show();
+        this.Close();
+    }
+
+    private void GO_PATIENT(object sender, RoutedEventArgs e)
+    {
+        PatientWindow windows = new PatientWindow();
+        windows.Show();
+        this.Close();
+    }
+}
