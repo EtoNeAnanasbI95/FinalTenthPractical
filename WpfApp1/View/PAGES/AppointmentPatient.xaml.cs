39808f3be735ab808ba9b830e46f71d2383dd719
pushechkin524 <134803783+pushechkin524@users.noreply.github.com>
2024-06-16 22:51:21 +0300

    Осталось реализовать страницу выбора времени и даты

--- WpfApp1/View/PAGES/AppointmentPatient.xaml.cs
+++ WpfApp1/View/PAGES/AppointmentPatient.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using FinalTenthPractical.View.USERCONTROLS;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace FinalTenthPractical.View.PAGES;
@@ -8,6 +9,71 @@ public partial class AppointmentPatient : Page
     public AppointmentPatient()
     {
         InitializeComponent();
+
+        DoctorsPatient th = new DoctorsPatient();
+        th.first.Text = "Jadsjskd";
+        DoctorsPatient qw = new DoctorsPatient();
+        qw.first.Text = "Jadsjskd";
+
+        DoctorsPatient tccvh = new DoctorsPatient();
+        tccvh.first.Text = "Jadsjskd";
+
+        DoctorsPatient sdsdsd = new DoctorsPatient();
+        sdsdsd.first.Text = "Jadsjskd";
+
+        DoctorsPatient ghjk = new DoctorsPatient();
+        ghjk.first.Text = "Jadsjskd";
+        
+        DoctorsPatient wefgbn = new DoctorsPatient();
+        wefgbn.first.Text = "Jadsjskd";
+        
+        DoctorsPatient txcvbhgtr4h = new DoctorsPatient();
+        txcvbhgtr4h.first.Text = "Jadsjskd";
+        
+        DoctorsPatient tsdhth = new DoctorsPatient();
+        tsdhth.first.Text = "Jadsjskd";
+
+        List<DoctorsPatient> users = new List<DoctorsPatient>() { th, qw, tccvh, sdsdsd, ghjk, wefgbn, txcvbhgtr4h, tsdhth };
+        LB1.ItemsSource = users;
+
+        MounthPatient tyui = new MounthPatient();
+        tyui.mounth.Text = "Хуябрь 2023";
+
+        MounthPatient jhsdksj = new MounthPatient();
+        jhsdksj.mounth.Text = "Хуябрь 2023";
+        
+        MounthPatient sdfc = new MounthPatient();
+        sdfc.mounth.Text = "Хуябрь 2023";
+        
+        MounthPatient defc = new MounthPatient();
+        defc.mounth.Text = "Хуябрь 2023";
+        
+        MounthPatient sdcx = new MounthPatient();
+        sdcx.mounth.Text = "Хуябрь 2023";
+
+        List<MounthPatient> mounthhh = new List<MounthPatient>() { tyui, jhsdksj, sdfc, defc, sdcx};
+        LB2.ItemsSource = mounthhh;
+
+
+        MounthPatient s = new MounthPatient();
+        tyui.mounth.Text = "Хуябрь 2023";
+
+        MounthPatient q = new MounthPatient();
+        jhsdksj.mounth.Text = "Хуябрь 2023";
+        
+        MounthPatient w = new MounthPatient();
+        sdfc.mounth.Text = "Хуябрь 2023";
+        
+        MounthPatient e = new MounthPatient();
+        defc.mounth.Text = "Хуябрь 2023";
+        
+        MounthPatient r = new MounthPatient();
+        sdcx.mounth.Text = "Хуябрь 2023";
+
+        List<MounthPatient> qqqqq = new List<MounthPatient>() { s, q, w, e, r};
+        LB3.ItemsSource = qqqqq;
+
+
     }
 
     private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
@@ -20,4 +86,7 @@ public partial class AppointmentPatient : Page
             autorized.Frame.Content = new MakeAnAppointmentPatient();
         }
     }
+
+
+
 }
---
+++ WpfApp1/View/USERCONTROLS/MounthPatient.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalTenthPractical.View.USERCONTROLS
+{
+    /// <summary>
+    /// Логика взаимодействия для MounthPatient.xaml
+    /// </summary>
+    public partial class MounthPatient : UserControl
+    {
+        public MounthPatient()
+        {
+            InitializeComponent();
+
+
+            MounthAppointmentPatient oihgf = new MounthAppointmentPatient();
+            oihgf.tbt.Text = "мать его ебал";
+
+            MounthAppointmentPatient vgyu = new MounthAppointmentPatient();
+            vgyu.tbt.Text = "мать его ебал";
+
+            List<MounthAppointmentPatient> users = new List<MounthAppointmentPatient>() {oihgf, vgyu };
+            LBBB.ItemsSource = users;
+        }
+    }
+}
