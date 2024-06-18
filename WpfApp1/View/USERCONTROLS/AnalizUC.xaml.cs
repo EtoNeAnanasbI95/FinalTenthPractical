a1e2e6a67e8b6560bf7acbe69d94a78af4695a4b
EtoNeAnanas95 <ananasovii.yashik@gmail.com>
2024-06-18 18:44:14 +0300

    добавил бэкенд к странице анализов

--- WpfApp1/View/USERCONTROLS/AnalizUC.xaml.cs
+++ WpfApp1/View/USERCONTROLS/AnalizUC.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EMIAS.Models;
 
 namespace FinalTenthPractical.View.USERCONTROLS
 {
@@ -20,9 +21,19 @@ namespace FinalTenthPractical.View.USERCONTROLS
     /// </summary>
     public partial class AnalizUC : UserControl
     {
+        public event EventHandler Click;
+        
         public AnalizUC()
         {
             InitializeComponent();
+            OnClick.Click += (sender, e) => GoClick(sender, e);
+        }
+
+        public Appointment Appointment;
+
+        private void GoClick(object sender, EventArgs e)
+        {
+            Click?.Invoke(this, e);
         }
     }
 }
