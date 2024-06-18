d9929b4e6582b2072fbd4f1633d0fe0c4ff5c7c4
pushechkin524 <134803783+pushechkin524@users.noreply.github.com>
2024-06-19 00:36:29 +0300

    Готов ВЕСЬ фронтенд!!!

---
+++ WpfApp1/View/PAGES/DateAndTimeOfAppintmentSecond.xaml.cs
@@ -0,0 +1,51 @@
+﻿using FinalTenthPractical.View.USERCONTROLS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalTenthPractical.View.PAGES
+{
+    /// <summary>
+    /// Логика взаимодействия для DateAndTimeOfAppintmentSecond.xaml
+    /// </summary>
+    public partial class DateAndTimeOfAppintmentSecond : Page
+    {
+        public DateAndTimeOfAppintmentSecond()
+        {
+
+
+            InitializeComponent();
+
+            DateOfAppointment aa = new DateOfAppointment();
+            aa.tbl.Text = "29 мая, ср";
+
+            DateOfAppointment aaa = new DateOfAppointment();
+            aaa.tbl.Text = "29 мая, ср";
+
+            DateOfAppointment aaaa = new DateOfAppointment();
+            aaaa.tbl.Text = "29 мая, ср";
+
+            DateOfAppointment aaaaa = new DateOfAppointment();
+            aaaaa.tbl.Text = "29 мая, ср";
+
+            DateOfAppointment aaaaaa = new DateOfAppointment();
+            aaaaaa.tbl.Text = "29 мая, ср";
+
+
+            List<DateOfAppointment> users = new List<DateOfAppointment>() { aa, aaa, aaaa, aaaaa, aaaaaa };
+            LB.ItemsSource = users;
+        }
+
+    }
+}
